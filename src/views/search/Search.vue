9901eb29c15565fe506c1e4480aa2490d5f376ea
1637923564 <1637923564@qq.com>
2020-08-27 11:13:43 +0800

    new feature

--- src/views/search/Search.vue
+++ src/views/search/Search.vue
@@ -65,20 +65,12 @@ export default {
     Confirm,
     TopTip
   },
-  data() {
-    return {
-      query: ''
-    }
-  },
   computed: {
     tag() {
       return this.hotKey.concat(this.searchHistory)
     }
   },
   methods: {
-    getQuery(query) {
-      this.query = query
-    },
     searchInputBlur() {
       this.$refs.input.blur()
     },
@@ -112,14 +104,6 @@ export default {
   },
   created() {
     this._getHotKey()
-  },
-  beforeRouteLeave (to, from, next) {
-    if (this.query) {
-      this.$refs.input.clearVal()
-      next(false)
-    } else {
-      next()
-    }
   }
 }
 </script>
